687efad8a324e7c038b890a2add491f13820f264
高見拓磨 <ksuper_stpreaaa@yahoo.co.jp>
2019-07-12 01:11:53 +0900

    make signup function

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
+
+  has_secure_password
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/index'
+  get 'pages/help'
+
+  resources :users
+end
